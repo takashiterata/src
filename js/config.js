7f8d909e1b51367fcca9816715f4606c90b8c1b7
takashiterata <t.terata@l-owcal.com>
2022-12-21 12:30:24 +0900

    adc

--- js/config.js
+++ js/config.js
@@ -66,7 +66,7 @@ jQuery.noConflict();
 
 
 //移動対象のリストを取得
-async  function FncListTable(PLUGIN_ID){
+async function FncListTable(PLUGIN_ID){
   try{
     let config = kintone.plugin.app.getConfig(PLUGIN_ID);
     const ListTable = document.getElementById("ListTable");
@@ -116,88 +116,90 @@ async  function FncListTable(PLUGIN_ID){
     console.log(error.message);
     window.alert("エラーが発生した為、処理をキャンセルしました。\n" + error.message);
   } finally {  //後処理
+    FncDragiven();
   }
 }
 
+function FncDragiven(e){
+  // アイテムのリストを取得
+  const items = [...document.querySelectorAll(".item")];
 
-// アイテムのリストを取得
-const items = [...document.querySelectorAll(".item")];
+  // ドラッグ開始イベントを定義
+  const handleDragStart = (e) => {
+    e.target.classList.add("dragging");
 
-// ドラッグ開始イベントを定義
-const handleDragStart = (e) => {
-  e.target.classList.add("dragging");
+    // ドロップ効果の設定
+    e.dataTransfer.effectAllowed = "move";
 
-  // ドロップ効果の設定
-  e.dataTransfer.effectAllowed = "move";
+    // 転送するデータの設定
+    const { id } = e.target;
+    e.dataTransfer.setData("application/json", JSON.stringify({ id }));
+  };
 
-  // 転送するデータの設定
-  const { id } = e.target;
-  e.dataTransfer.setData("application/json", JSON.stringify({ id }));
-};
+  // ドラッグ終了イベントを定義
+  const handleDragEnd = (e) => e.target.classList.remove("dragging");
 
-// ドラッグ終了イベントを定義
-const handleDragEnd = (e) => e.target.classList.remove("dragging");
-
-// アイテムにイベントを登録
-for (const item of items) {
-  item.addEventListener("dragstart", handleDragStart, false);
-  item.addEventListener("dragend", handleDragEnd, false);
-}
-
-// 要素が重なった際のイベントを定義
-const handleDragEnter = (e) => {
-  // 子要素へのドラッグを制限
-  if ([...e.target.classList].includes("item")) {
-    return;
+  // アイテムにイベントを登録
+  for (const item of items) {
+    item.addEventListener("dragstart", handleDragStart, false);
+    item.addEventListener("dragend", handleDragEnd, false);
   }
 
-  e.target.classList.add("over");
-};
+  // 要素が重なった際のイベントを定義
+  const handleDragEnter = (e) => {
+    // 子要素へのドラッグを制限
+    if ([...e.target.classList].includes("item")) {
+      return;
+    }
 
-// 要素が離れた際のイベントを定義
-const handleDragLeave = (e) => e.target.classList.remove("over");
+    e.target.classList.add("over");
+  };
 
-// 要素が重なっている最中のイベントを定義
-const handleDragOver = (e) => {
-  // 要素が重なった際のブラウザ既定の処理を変更
-  e.preventDefault();
+  // 要素が離れた際のイベントを定義
+  const handleDragLeave = (e) => e.target.classList.remove("over");
 
-  // 子要素へのドラッグを制限
-  if ([...e.target.classList].includes("item")) {
-    // ドラッグ不可のドロップ効果を設定
-    e.dataTransfer.dropEffect = "none";
-    return;
+  // 要素が重なっている最中のイベントを定義
+  const handleDragOver = (e) => {
+    // 要素が重なった際のブラウザ既定の処理を変更
+    e.preventDefault();
+
+    // 子要素へのドラッグを制限
+    if ([...e.target.classList].includes("item")) {
+      // ドラッグ不可のドロップ効果を設定
+      e.dataTransfer.dropEffect = "none";
+      return;
+    }
+
+    // ドロップ効果の設定
+    e.dataTransfer.dropEffect = "move";
+  };
+
+  // 要素がドロップされた際のイベントを定義
+  const handleDrop = (e) => {
+    // 要素がドロップされた際のブラウザ既定の処理を変更
+    e.preventDefault();
+    e.target.classList.remove("over");
+
+    // ブラウザ外からのファイルドロップを制限
+    if (e.dataTransfer.files.length > 0) {
+      return;
+    }
+
+    // 転送データの取得
+    const { id } = JSON.parse(e.dataTransfer.getData("application/json"));
+
+    // ドロップ先に要素を追加する
+    e.target.appendChild(document.getElementById(id));
+  };
+
+  // ドロップ先のリストを取得
+  const boxes = [...document.querySelectorAll(".box")];
+
+  // ドロップ先にイベントを登録
+  for (const box of boxes) {
+    box.addEventListener("dragenter", handleDragEnter, false);
+    box.addEventListener("dragleave", handleDragLeave, false);
+    box.addEventListener("dragover", handleDragOver, false);
+    box.addEventListener("drop", handleDrop, false);
   }
-
-  // ドロップ効果の設定
-  e.dataTransfer.dropEffect = "move";
-};
-
-// 要素がドロップされた際のイベントを定義
-const handleDrop = (e) => {
-  // 要素がドロップされた際のブラウザ既定の処理を変更
-  e.preventDefault();
-  e.target.classList.remove("over");
-
-  // ブラウザ外からのファイルドロップを制限
-  if (e.dataTransfer.files.length > 0) {
-    return;
-  }
-
-  // 転送データの取得
-  const { id } = JSON.parse(e.dataTransfer.getData("application/json"));
-
-  // ドロップ先に要素を追加する
-  e.target.appendChild(document.getElementById(id));
-};
-
-// ドロップ先のリストを取得
-const boxes = [...document.querySelectorAll(".box")];
-
-// ドロップ先にイベントを登録
-for (const box of boxes) {
-  box.addEventListener("dragenter", handleDragEnter, false);
-  box.addEventListener("dragleave", handleDragLeave, false);
-  box.addEventListener("dragover", handleDragOver, false);
-  box.addEventListener("drop", handleDrop, false);
 }
