9630641395b0195603818a7dc36887c3533faafa
takashiterata <t.terata@l-owcal.com>
2022-12-27 18:01:46 +0900

    dg

--- js/config.js
+++ js/config.js
@@ -171,33 +171,13 @@ async function FncListTable(PLUGIN_ID){
           }else if(layout[ii]['type'] == 'GROUP'){
             for(let i4=0;i4 <layout[ii]['layout'].length;i4++){
               for(let i5=0;i5 <layout[ii]['layout'][i4]['fields'].length;i5++){
-                if(layout[ii]['layout'][i4]['fields'][i5]['type']=='SPACER'){
-                  tabsetvalmid[tabsetval2[1]] += FncClehtml(layout[ii]['layout'][i4]['fields'][i5]);
-                }else if(layout[ii]['layout'][i4]['fields'][i5]['type']=='LABEL'){
-                  tabsetvalmid[tabsetval2[1]] += FncClehtml(layout[ii]['layout'][i4]['fields'][i5]);
-                }else if(layout[ii]['layout'][i4]['fields'][i5]['type']=='HR'){
-                  tabsetvalmid[tabsetval2[1]] += FncClehtml(layout[ii]['layout'][i4]['fields'][i5]);
-                }else{
-                  tabsetvalmid[tabsetval2[1]] += FncClehtml(layout[ii]['layout'][i4]['fields'][i5]);
-                }
+                tabsetvalmid[tabsetval2[1]] += FncClehtml(layout[ii]['layout'][i4]['fields'][i5]);
               }
             }
           }else{
-            tabsetvalmid[tabsetval2[1]] += '<div class="row-gaia clearFix-cybozu" style="position: relative;">';
+            tabsetvalmid[tabsetval2[1]] += '<div class="" style="position: relative;">';
             for(let i4=0;i4 <layout[ii]['fields'].length;i4++){
-              if(layout[ii]['fields'][i4]['type']=='SPACER'){
-                tabsetvalmid[tabsetval2[1]] += FncClehtml(layout[ii]['fields'][i4]);
-                //tabsetvalmid[tabsetval2[1]] += 'スペース　　';
-              }else if(layout[ii]['fields'][i4]['type']=='LABEL'){
-                tabsetvalmid[tabsetval2[1]] += FncClehtml(layout[ii]['fields'][i4]);
-                //tabsetvalmid[tabsetval2[1]] += 'ラベル　　';
-              }else if(layout[ii]['fields'][i4]['type']=='HR'){
-                tabsetvalmid[tabsetval2[1]] += FncClehtml(layout[ii]['fields'][i4]);
-                //tabsetvalmid[tabsetval2[1]] += '罫線　　';
-              }else{
-                tabsetvalmid[tabsetval2[1]] += FncClehtml(layout[ii]['fields'][i4]);
-                //tabsetvalmid[tabsetval2[1]] += layout[ii]['fields'][i4]['code'] + '　　';
-              }
+              tabsetvalmid[tabsetval2[1]] += FncClehtml(layout[ii]['fields'][i4]);
             }
             tabsetvalmid[tabsetval2[1]] += '</div>';
           }
