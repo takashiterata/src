c571aa84e1efe53d1ee2cb0672daa07fa3dae34f
takashiterata <t.terata@l-owcal.com>
2022-12-27 13:53:59 +0900

    あ

--- js/config.js
+++ js/config.js
@@ -163,7 +163,17 @@ async function FncListTable(PLUGIN_ID){
         let iii = ii +1;
         let tabsetval2 = tabsetval[ii].split('--');
         if(tabsetval2[0] == i){
-          tabsetvalmid[tabsetval2[1]]= '<div class="item" draggable="true" id="item' + iii +'">' +iii + '行目' + layout[ii]['type'] + iii +'</div>';
+          tabsetvalmid[tabsetval2[1]] += '<div class="item" draggable="true" id="item' + iii +'">' +iii + '行目' + layout[ii]['type'] + iii;
+          if(layout[ii]['type'] == 'SUBTABLE'){
+            tabsetvalmid[tabsetval2[1]] += '';
+          }else if(layout[i]['type'] == 'GROUP'){
+            tabsetvalmid[tabsetval2[1]] += '';
+          }else{
+            for(let i4=0;i4 <layout[i]['fields'].length;i4++){
+              tabsetvalmid[tabsetval2[1]] += layout[ii]['type']['code'] + '　　';
+            }
+          }
+          tabsetvalmid[tabsetval2[1]] += '</div>';
         }
       }
       for(let ii=0;ii<tabsetvalmid.length;ii++){
