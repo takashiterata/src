a007e6804b7ab8dc8a37f755f6ee4686e68f2040
takashiterata <t.terata@l-owcal.com>
2022-12-28 16:16:41 +0900

    adv

--- js/config.js
+++ js/config.js
@@ -469,7 +469,7 @@ if(valhtml['type']=='SPACER'){
   htmlaa += '</div>';
 }else if(valtype == 'SUBTABLE'){
   htmlaa += '<div class="" style="box-sizing: border-box; width: ' + valhtml['size']['width'] + 'px; height: auto;">';
-  htmlaa += '<div class="" style=""><span class="control-label-text-gaia">' + gproperties[valname][fields][valhtml['code']].label + '</span></div>';
+  htmlaa += '<div class="" style=""><span class="control-label-text-gaia">' + gproperties[valname]['fields'][valhtml['code']].label + '</span></div>';
   htmlaa += '<div class="control-value-gaia"><span class="control-value-content-gaia">'+ valhtml['code'] +'</span></div>';
   htmlaa += '<div class=""></div>';
   htmlaa += '</div>';
